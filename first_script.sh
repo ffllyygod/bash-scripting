cf5915db69e5fe609d186d6f2d9270cb534790b8
Ayush Abad <yush@pop-os.localdomain>
2024-07-13 15:13:19 +0530

    Adding scripts

---
+++ first_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Welcome to bash script"
+echo
+
+echo "############################################"
+echo "The uptime for the system is"
+uptime
+echo
+
+echo "############################################"
+echo "Memory Utilization"
+free -m
+echo
+
+echo "###########################################"
+echo "Disk Utilization"
+df -h
+echo
